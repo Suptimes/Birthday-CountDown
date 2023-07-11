74ad17bf4b4ea00e94cbc6a9af25c6a18b4af57a
Suptimes <133566100+Suptimes@users.noreply.github.com>
2023-07-11 13:57:53 +0400

    commit photo galerie scroll

--- script.js
+++ script.js
@@ -51,3 +51,43 @@ envelopeFull.addEventListener("click", function openCloseEnv(){
           envelope.className = "close";
         }
 })
+
+// Images Panorama ---------------------------------
+
+const track = document.querySelector('.image-track')
+
+window.onmousedown = e => {
+  track.dataset.mouseDownAt = e.clientX
+}
+
+window.onmousemove = e => {
+  if(track.dataset.mouseDownAt === '0') return
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX
+  const maxDelta = window.innerWidth / 2
+
+  const percentage = (mouseDelta / maxDelta) * -100
+  // const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage
+  
+  const nextPercentage = Math.max(-100, Math.min(0, parseFloat(track.dataset.prevPercentage) + percentage));
+  
+
+
+  track.dataset.percentage = nextPercentage
+
+  track.style.transform = `translate(${nextPercentage}%, 0%)`
+
+  for(const image of track.getElementsByClassName("image")){
+    track.animate({
+      transform: `translate(${nextPercentage}%, 0%)`},{ duration: 1200, fill: "forwards" })
+    
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`}, { duration: 1200, fill: "forwards"})
+  }
+}
+
+window.onmouseup = () => {
+  track.dataset.mouseDownAt = '0'
+  track.dataset.prevPercentage = track.dataset.percentage
+}
+
